c76301a2e108d6d7d0cdfc316101cb4b18dbee13
insanj <insanj@gmail.com>
2015-07-14 01:40:44 -0400

    removed unneeded files and setup xcodeproj for 6.0

---
+++ Accordian/Accordian/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Accordian
+//
+//  Created by Julian Weiss on 7/14/15.
+//  Copyright (c) 2015 Julian Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Accordian/Accordian/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Accordian
+//
+//  Created by Julian Weiss on 7/14/15.
+//  Copyright (c) 2015 Julian Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
